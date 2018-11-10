bd58bda934941db92cc5943bbc01c39c1b20f1ae
JJ Benson <james@Emmas-MacBook-Pro-2.local>
2018-11-10 16:35:13 +0000

    "Game Logic updated"

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,21 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
+//MAIN ARRAY
+var cards = [ "queen", "queen", "king", "king" ];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push( cardOne );
+cardsInPlay.push( cardTwo );
+
+console.log( "User flipped "+ cardOne );
+console.log( "User flipped "+ cardTwo );
+
+if ( cardsInPlay.length === 2 ) {
+	if ( cardsInPlay[0] === cardsInPlay[1] ) {
+		alert( "You found a match!" );
+	} else {
+		alert( "Sorry try again!" );
+	}
+
+}
