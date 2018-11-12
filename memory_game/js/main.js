3f4fe5a1ee4aad142e612a92e9b53bf502101255
jjbenson85@gmail.com <james@Emmas-MacBook-Pro-2.local>
2018-11-12 12:55:41 +0000

    Added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,20 +2,29 @@
 var cards = [ "queen", "queen", "king", "king" ];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
+var flipCard = function(cardId){
+	/*var cardOne = cards[0];
+	var cardTwo = cards[1];
 
-cardsInPlay.push( cardOne );
-cardsInPlay.push( cardTwo );
+	cardsInPlay.push( cardOne );
+	cardsInPlay.push( cardTwo );
 
-console.log( "User flipped "+ cardOne );
-console.log( "User flipped "+ cardTwo );
+	console.log( "User flipped "+ cardOne );
+	console.log( "User flipped "+ cardTwo );*/
 
-if ( cardsInPlay.length === 2 ) {
-	if ( cardsInPlay[0] === cardsInPlay[1] ) {
-		alert( "You found a match!" );
-	} else {
-		alert( "Sorry try again!" );
+	console.log( "User flipped " + cards[cardId]);
+	cardsInPlay.push( cards[cardId] );
+
+	if ( cardsInPlay.length === 2 ) {
+		if ( cardsInPlay[0] === cardsInPlay[1] ) {
+			alert( "You found a match!" );
+		} else {
+			alert( "Sorry try again!" );
+		}
+
 	}
 
 }
+
+flipCard(0);
+flipCard(2);
